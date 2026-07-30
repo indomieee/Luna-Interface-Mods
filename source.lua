5ac46acae7f8c5421220561ff772b6f84773ff1b
indomieee <muhammadaldi.ma47@gmail.com>
2026-07-30 08:43:42 +0700

    change divider v4

--- source.lua
+++ source.lua
@@ -4479,13 +4479,12 @@ function Tab:CreateDivider(name)
 
 	-- Transparan di pinggir, terang di tengah
 	gradient.Transparency = NumberSequence.new({
-		NumberSequenceKeypoint.new(0.00, 1.00),
-		NumberSequenceKeypoint.new(0.15, 0.85),
-		NumberSequenceKeypoint.new(0.35, 0.55),
-		NumberSequenceKeypoint.new(0.50, 0.50),
-		NumberSequenceKeypoint.new(0.65, 0.55),
-		NumberSequenceKeypoint.new(0.85, 0.85),
-		NumberSequenceKeypoint.new(1.00, 1.00),
+	        NumberSequenceKeypoint.new(0.00, 1.00),
+	        NumberSequenceKeypoint.new(0.20, 0.75),
+	        NumberSequenceKeypoint.new(0.40, 0.05),
+	        NumberSequenceKeypoint.new(0.60, 0.05),
+	        NumberSequenceKeypoint.new(0.80, 0.75),
+	        NumberSequenceKeypoint.new(1.00, 1.00),
 	})
 
 	------------------------------------------------
